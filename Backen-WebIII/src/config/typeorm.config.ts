079083fe622e95289f32ab125ba34fdf5ad38a0b
DannyFlores <df2720298@gmail.com>
2025-11-03 09:54:11 -0700

    Agregacion de DATABASE_URL

--- Backen-WebIII/src/config/typeorm.config.ts
+++ Backen-WebIII/src/config/typeorm.config.ts
@@ -4,9 +4,10 @@ import { join } from "path";
 
 export const typeOrmConfig = (configService: ConfigService): TypeOrmModuleOptions => ({
     type: "postgres",
-    host: configService.get<string>("DB_HOST", 'localhost'),
+    host: configService.get<string>("DB_HOST", 'dpg-d41l7gfgi27c739kshsg-a.oregon-postgres.render.com'),
+    url: configService.get<string>("DATABASE_URL"),
     port: +configService.get<string>('DB_PORT', '5432'),
-    username: configService.get<string>("DB_USERNAME", 'postgres'),
+    username: configService.get<string>("DB_USERNAME", 'candyland_db_user'),
     password: configService.get<string>("DB_PASSWORD"),
     database: configService.get<string>("DB_NAME"),
     entities: [join(__dirname, '../**/*.entity{.ts,.js}')],
