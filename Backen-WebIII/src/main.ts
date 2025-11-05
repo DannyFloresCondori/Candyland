0052700096035e4d79a4082d0b0d3368b7d100fa
DannyFlores <df2720298@gmail.com>
2025-11-04 20:51:54 -0700

    cambiando cors

--- Backen-WebIII/src/main.ts
+++ Backen-WebIII/src/main.ts
@@ -10,7 +10,10 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://candyland-five.vercel.app'
+    ],
     credentials: true,
   });
 
